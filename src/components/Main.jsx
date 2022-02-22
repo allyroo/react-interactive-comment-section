13a3969bb63f2cc90ca5c984b44bd6d4ec02f33f
Allyse Arucan <allyse.arucan@gmail.com>
2022-02-21 22:54:47 -0700

    moved img file + added data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,23 +6,30 @@ const Comment = ({ comment }) => {
   return (
     <div className="commentBox" key={comment.id}>
       <div className="commentContent">
+        <h3>{comment.user.username}</h3>
+        <h4>{comment.createdAt}</h4>
+        <img src={comment.user.image.png} alt="" />
         <div className="comment">{comment.content}</div>
-
       </div>
+
       {comment.replies.map((reply) => (
-          <div key={reply.id}>{reply.content}</div>
-        ))}
+        <div key={reply.id}>
+          <h3>{reply.user.username}</h3>
+          <h4>{reply.createdAt}</h4>
+          <img src={reply.user.image.webp} alt="" />
+          <p>{reply.content}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const Main = () => {
-  console.log(data);
-  // let commentOne = data.comments[0].content;
-
   return (
     <div className="wrapper">
-      {data.comments.map((comment) => <Comment comment={comment} />)}
+      {data.comments.map((comment) => (
+        <Comment comment={comment} />
+      ))}
     </div>
   );
 };
